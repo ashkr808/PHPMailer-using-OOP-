71af1797a3337f167814def006a13059e94e6e70
Ashish Patel <51435216+ashishkr143@users.noreply.github.com>
2020-06-19 09:36:10 +0530

    Fixed : Re-Instantiating of object for sending mail to different reciever.
    I separated the receiver email, body, subject from constructor and added these to sendMail() function.

--- Mail.php
+++ Mail.php
@@ -3,28 +3,22 @@
         private $senderName;
         private $senderEmail;
         private $password;
-        private $reciever;
-        private $subject;
-        private $body;
         private $SMTPhost;
 
-        public function __construct($senderName,$senderEmail,$password,$reciever,$subject,$body,$SMTPhost = 'smtp.gmail.com')
+        public function __construct($senderName,$senderEmail,$password,$SMTPhost = 'smtp.gmail.com')
         {
             require('phpmailer/PHPMailerAutoload.php');
             $this->senderName = $senderName;
             $this->senderEmail = $senderEmail;
             $this->password = $password;
-            $this->reciever = $reciever;
-            $this->subject = $subject;
-            $this->body = $body;
             $this->SMTPhost = $SMTPhost;
         }
-        public function sendMail(){
+        public function sendMail($reciever,$subject = "Test subject",$body = "Test body"){
 
             $mail = new PHPMailer;
             $mail->SMTPDebug = 0;                               // Enable verbose debug output
             $mail->isSMTP();                                      // Set mailer to use SMTP
-            $mail->Host = $this->SMTPhost;                            // Specify main and backup SMTP servers
+            $mail->Host =  $this->SMTPhost;                            // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
             $mail->Username = $this->senderEmail;                 // SMTP username
             $mail->Password = $this->password;                           // SMTP password
@@ -32,15 +26,15 @@
             $mail->Port = 587;                                    // TCP port to connect to
 
             $mail->setFrom($this->senderEmail, $this->senderName);
-            $mail->addAddress($this->reciever);                     // Add a recipient
+            $mail->addAddress($reciever);                     // Add a recipient
 
             $mail->addReplyTo($this->senderEmail);
         
             $mail->isHTML(true);                                  // Set email format to HTML
 
-            $mail->Subject = $this->subject;
-            $mail->Body    = $this->body;
-            $mail->AltBody = $this->body;
+            $mail->Subject = $subject;
+            $mail->Body    = $body;
+            $mail->AltBody = $body;
             $mail->send();
         }
     }
